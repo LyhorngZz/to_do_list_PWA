68991591d4a307e1edafcb53d4c1904e4346eeef
Raby001 <hanraby001@gmail.com>
2026-07-06 10:20:08 +0700

    Structure the Work

---
+++ apps/frontend/src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { onMounted } from 'vue';
+import api from './services/api';
+
+onMounted(async () => {
+  try {
+    const response = await api.get('/');
+
+    console.log(response.data);
+  } catch (err) {
+    console.error(err);
+  }
+});
+</script>
+
+<template>
+  <h1>Todo Offline App</h1>
+</template>
