7cb768ed6d3907ee790ce36d1a09c7bd80a6735d
LyhorngZz <e20220188@dtc.itc.edu.kh>
2026-07-08 15:25:49 +0700

    feat: add PWA support with Vite PWA plugin and icons
    - Updated package.json to include vite-plugin-pwa.
    - Added PWA icons to the public directory.
    - Registered service worker in main.ts for PWA functionality.
    - Created vite-env.d.ts for type definitions related to Vite and PWA.
    - Configured Vite to use PWA plugin with manifest and Workbox settings.

--- apps/frontend/src/main.ts
+++ apps/frontend/src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { registerSW } from 'virtual:pwa-register'
 
 import App from './App.vue'
 import router from './router'
@@ -11,3 +12,7 @@ app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
+
+registerSW({
+    immediate: true,
+});
--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,6 +12,47 @@ export default defineConfig({
     vue(),
     vueDevTools(),
     tailwindcss(),
+
+    VitePWA({
+      registerType: "autoUpdate",
+
+      devOptions: {
+        enabled: true,
+      },
+
+      manifest: {
+        name: "Todo PWA",
+        short_name: "Todo",
+
+        description:
+          "Offline-first Todo application built with Vue, RxDB and IndexedDB.",
+
+        theme_color: "#2563EB",
+        background_color: "#FFFFFF",
+
+        display: "standalone",
+
+        start_url: "/",
+
+        icons: [
+          {
+            src: "todo_pwa_icon_1.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "todo_pwa_icon_2.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+      workbox: {
+        globPatterns: [
+          "**/*.{js,css,html,ico,png,svg,woff2}"
+        ],
+      },
+    }),
   ],
   resolve: {
     alias: {
